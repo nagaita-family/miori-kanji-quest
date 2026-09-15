2baadb3a97f47ae5f3627bf74f292cfcc8913065
nagaitashouten-star <nagaita.shouten@gmail.com>
2026-09-15 13:37:18 +0900

    Refresh v2.5.2 eraser icon asset

--- app-v240-release.js
+++ app-v240-release.js
@@ -8,5 +8,5 @@
   const oldHome=renderHome;renderHome=function(){oldHome();refresh();setTimeout(refresh,90);};
   function load(src,done){if(document.querySelector(`script[data-v252="${src}"]`)){done?.();return;}const s=document.createElement('script');s.src=src;s.dataset.v252=src;s.onload=()=>done?.();s.onerror=()=>console.error('Kanji Quest: failed to load',src);document.body.appendChild(s);}
   refresh();
-  load('./app-v250-island.js?v=2520',()=>load('./app-v250-study.js?v=2520',()=>load('./app-v252-cell-erase.js?v=2520',()=>{refresh();window.MioriV250?.polishIsland?.();})));
+  load('./app-v250-island.js?v=2520',()=>load('./app-v250-study.js?v=2520',()=>load('./app-v252-cell-erase.js?v=2521',()=>{refresh();window.MioriV250?.polishIsland?.();})));
 })();
